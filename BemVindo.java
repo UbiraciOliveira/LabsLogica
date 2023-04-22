2bfa9b986f2d28097aa159f59b083a66549b6ee9
UbiraciOliveira <ubira.domingos@gmail.com>
2023-04-22 18:28:52 -0300

    Atualizando com novos comandos

---
+++ BemVindo.java
@@ -0,0 +1,8 @@
+
+public class BemVindo {
+    public static void main(String[] args) {
+
+        System.out.printf("Olá seja bem vindo!");
+
+    }
+}
---
+++ CalculadorDiferencaIdade.java
@@ -0,0 +1,11 @@
+public class CalculadorDiferencaIdade {
+
+    public static void main (String[] args){
+        int primeiraIdade = 30;
+        int segundaIdade = 52;
+        int diferencaIdade = primeiraIdade - segundaIdade;
+        System.out.println("Diferença entre idade é de: " + diferencaIdade);
+
+    }
+
+    }
